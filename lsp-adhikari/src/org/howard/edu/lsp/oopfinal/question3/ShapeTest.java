20691830f10402ec1f2d15b45d92e9057e793ea8
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-05-02 20:19:38 -0400

    Added assertTrue for testing as NotNUll could mean any shapes.

--- lsp-adhikari/src/org/howard/edu/lsp/oopfinal/question3/ShapeTest.java
+++ lsp-adhikari/src/org/howard/edu/lsp/oopfinal/question3/ShapeTest.java
@@ -1,18 +1,20 @@
 package org.howard.edu.lsp.oopfinal.question3;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 public class ShapeTest {
     @Test
     public void testCreateCircle() {
         Shape circle = ShapeFactory.createShape("circle");
         assertNotNull(circle);
+        assertTrue(circle instanceof Circle);
     }
 
     @Test
     public void testCreateRectangle() {
         Shape rectangle = ShapeFactory.createShape("rectangle");
         assertNotNull(rectangle);
+        assertTrue(rectangle instanceof Rectangle);
     }
 }
